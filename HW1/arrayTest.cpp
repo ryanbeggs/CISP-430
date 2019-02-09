56e3a751d01d03a2a62e204490ceb046c8748674
Ryan Beggs <ryanebeggs@gmail.com>
2019-02-08 16:37:40 -0800

    hw1 complete

--- HW1/arrayTest.cpp
+++ HW1/arrayTest.cpp
@@ -7,50 +7,73 @@
 
 using namespace std;
 
+//Function Prototype
 void greeting();
 
 int main() {
 
 	greeting();
 
-  int *myArray = new int[1] {1};
-  int runs = 100000;
-  int count = 1;
+  int *myArray = nullptr;       // initialize pointer to null
+  int size = 1;                 // array size
+  myArray = new int[size] {1};  // build new array on the heap, initialize to 1
+  int runs = 10000;             // size of final array
 
-  auto start = chrono::high_resolution_clock::now();
+  auto start = chrono::high_resolution_clock::now();  // start time
 
   for (int i = 0; i < runs; i++) {
 
-    int *tempArray = new int[count + 1];
+    int *tempArray = nullptr;                         // initialize pointer to null
+    tempArray = new int[size + 1];                    // build new array on the heap
 
-    cout << "Current array: " << i + 1 << "\r";
+    cout << "Current array: " << i + 1 << "\r";       // display progress
   
-    for (int j = 0; j < count; j++) {
+    for (int j = 0; j < size; j++) {                  // copy original array to temp array
       tempArray[j] = myArray[j];
     }
 
-    tempArray[count] = 1;
-
-    delete[] myArray;
-
-    myArray = tempArray;
-
-    count++;
-
+    tempArray[size] = 1;                              // add int to new temp array
+    delete[] myArray;                                 // free memory
+    myArray = tempArray;                              // swap pointers
+    tempArray = nullptr;                              // null tempArray
+    size++;                                           // increment size
   }
 
-  auto end = chrono::high_resolution_clock::now();
+  auto end = chrono::high_resolution_clock::now();    // end time
 
+  // calculate elapsed time in nanoseconds
   double elapsedTime = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
-  cout << "It took " << elapsedTime / 1000000000 << " seconds" << " to process " << runs << " elements" << endl;  
+  // display elapsed time in seconds
+  cout << "It took " << elapsedTime / 1000000000 << " seconds to process " << runs << " elements" << endl;  
 
   return 0;
 }
 
+//Program Greeting
 void greeting(){
 
-	cout << "blah" << endl;
-
+  cout << "**************************************************" << endl;
+	cout << "* This program calculates how long it takes to   *" << endl;
+  cout << "* repeatedly resize an array and add one element *" << endl;
+  cout << "* to the end of the array each time              *" << endl;
+  cout << "**************************************************" << endl;
 }
 
+/* Results
+Average of 3 runs without -O3 optimization
+
+     1,000 - 0.007219 Seconds
+    10,000 - 0.132090 Seconds
+   100,000 - 15.03343 Seconds
+ 1,000,000 - 1693.866 Seconds
+10,000,000 - Too long, unable to complete
+
+Average of 3 runs with -O3 optimization 
+
+     1,000 - 0.006465 Seconds
+    10,000 - 0.068023 Seconds
+   100,000 - 9.205700 Seconds
+ 1,000,000 - 1152.800 Seconds
+10,000,000 - Too long, unable to complete
+*/
